e50e5e816bf422eb32792e7c684c2d58f40e1417
Graeme Yeates <yeatesgraeme@gmail.com>
2016-04-18 15:10:07 -0400

    Fix #7 ramda node module resolve logic closes #8, closes #9

--- src/modules.js
+++ src/modules.js
@@ -1,7 +1,25 @@
+import {contains, merge} from 'ramda';
+import fs from 'fs';
+import Module from 'module';
 import path from 'path';
-import ramda, {contains, keys} from 'ramda';
 
-export let methods = keys(ramda);
+function getDirectories(srcPath) {
+  // Slow synchronous version of https://github.com/megawac/lodash-modularize/blob/master/src/lodashModules.js.
+  // Using the paths lodash-cli provides is not an option as they may change version to version =(
+  return ['.'].concat(fs.readdirSync(srcPath)).filter(filePath =>
+    fs.statSync(path.join(srcPath, filePath)).isDirectory());
+}
+
+const _ramdaPath = path.dirname(Module._resolveFilename('ramda', merge(new Module, {
+  'paths': Module._nodeModulePaths(process.cwd())
+})));
+
+// ramda folder will be /nodemodules/ramda/dist. We want to remove the dist
+const ramdaPath = _ramdaPath.slice(0, _ramdaPath.lastIndexOf('ramda') + 5);
+
+var methods = fs.readdirSync(path.join(ramdaPath, 'src'))
+    .filter(name => path.extname(name) == '.js')
+    .map(name => path.basename(name, '.js'));
 
 export default function resolveModule(name) {
 
