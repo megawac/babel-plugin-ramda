8ff566c4a98e66f2593cccf1278f0de4f2a534d1
alef <alef@whoisdrwho.net>
2016-06-19 15:56:50 +0300

    bugfix(index.js): fix property rewriting

--- test/fixtures/ramda-mixed-native/expected.js
+++ test/fixtures/ramda-mixed-native/expected.js
@@ -17,11 +17,26 @@ var mapper = (0, _map2.default)((0, _add2.default)(1));
 mapper([1, 2, 3]);
 
 [1, 2, 3].map(function (a) {
-	return a + 1;
+  return a + 1;
 });
 
 var obj = {
-	map: 1
+  map: 1
 };
 
 var obj1 = _defineProperty({}, _add2.default, 2);
+
+var ob2 = {
+  rate: _add2.default
+};
+
+function test() {
+  var param = arguments.length <= 0 || arguments[0] === undefined ? _map2.default : arguments[0];
+
+  return param;
+}
+
+var test1 = function test1() {
+  var param2 = arguments.length <= 0 || arguments[0] === undefined ? _add2.default : arguments[0];
+  return _add2.default;
+};
