3f2550cb5ae931f5bb3da37a631a5343bf9ef496
Mathieu Darse <mdarse@jolicode.com>
2017-01-12 23:40:39 +0100

    Only rewrite bindings to ramda functions

---
+++ test/fixtures/scope-shadowing/expected.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var _prop = require('ramda/src/prop');
+
+var _prop2 = _interopRequireDefault(_prop);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function bar(R, prop, foo) {
+  R.prop(foo);
+  foo;
+  foo();
+}
+function baz() {
+  (0, _prop2.default)(_prop2.default);
+  _prop2.default;
+  (0, _prop2.default)();
+}
