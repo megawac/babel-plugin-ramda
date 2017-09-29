084d0fdb4593d20650ceab5ff25e83473324d27b
Harry Hogg <harry@hogg.io>
2017-09-29 09:28:27 +0100

    test(specifier-alias): Correct alias test fixtures

--- test/fixtures/specifier-alias/expected.js
+++ test/fixtures/specifier-alias/expected.js
@@ -4,8 +4,13 @@ var _add = require('ramda/src/add');
 
 var _add2 = _interopRequireDefault(_add);
 
+var _map = require('ramda/src/map');
+
+var _map2 = _interopRequireDefault(_map);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var mapper = map((0, _add2.default)(1));
+var map = function map() {};
+var mapper = map((0, _map2.default)((0, _add2.default)(1)));
 
 mapper([1, 2, 3]);
