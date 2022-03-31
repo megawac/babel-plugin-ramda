eb0d800c07fdd740064d6dffc2f21480bc1e91cb
Josh Crites <josh@vint.co>
2022-03-31 14:50:18 -0400

    update plugin to support new versions of ramda
    commit d100c9549c92d757447dfaa7d142ab451c6ce3e0
    Author: Josh Crites <josh@vint.co>
    Date:   Thu Mar 31 14:45:32 2022 -0400
    
        update use of deprecated functions, use const, remove unnecessary loop
    
        Signed-off-by: Josh Crites <josh@vint.co>
    
    commit ba5f35e46d80d36d55a679c0db7e0ec61e071e0c
    Author: Josh Crites <josh@vint.co>
    Date:   Thu Mar 31 14:44:28 2022 -0400
    
        update ramda dep/min version, package.json version, package registry
    
        Signed-off-by: Josh Crites <josh@vint.co>
    
    Signed-off-by: Josh Crites <josh@vint.co>

--- src/modules.js
+++ src/modules.js
@@ -1,4 +1,4 @@
-import {contains, merge} from 'ramda';
+import {includes, mergeRight} from 'ramda';
 import fs from 'fs';
 import Module from 'module';
 import path from 'path';
@@ -10,7 +10,7 @@ function getDirectories(srcPath) {
     fs.statSync(path.join(srcPath, filePath)).isDirectory());
 }
 
-const _ramdaPath = path.dirname(Module._resolveFilename('ramda', merge(new Module, {
+const _ramdaPath = path.dirname(Module._resolveFilename('ramda', mergeRight(new Module, {
   'paths': Module._nodeModulePaths(process.cwd())
 })));
 
@@ -19,16 +19,13 @@ const ramdaPath = _ramdaPath.slice(0, _ramdaPath.lastIndexOf('ramda') + 5);
 
 // We do not need to change the search path based on useES since src and es are both built from the
 // same source in Ramda, and the directories will therefore always have identical contents.
-var methods = fs.readdirSync(path.join(ramdaPath, 'src'))
-    .filter(name => path.extname(name) == '.js')
+const methods = fs.readdirSync(path.join(ramdaPath, 'src'))
+    .filter(name => path.extname(name) === '.js')
     .map(name => path.basename(name, '.js'));
 
 export default function resolveModule(useES, name) {
-
-  for (var category in methods) {
-    if (contains(name, methods)) {
-      return `ramda/${useES ? 'es' : 'src'}/${name}`;
-    }
+  if (includes(name, methods)) {
+    return `ramda/${useES ? 'es' : 'src'}/${name}`;
   }
   throw new Error(`Ramda method ${name} was not a known function
     Please file a bug if it's my fault https://github.com/megawac/babel-plugin-ramda/issues
